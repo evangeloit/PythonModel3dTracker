7791b5de0260caaab08a2305a55b1cd1e6405eb4
Alexandros Makris <makris.alexandros@gmail.com>
2018-02-06 18:37:21 +0200

    minor diffs.

--- Scripts/MoreScripts/experiments/Levmar17/LaunchExperiments.py
+++ Scripts/MoreScripts/experiments/Levmar17/LaunchExperiments.py
@@ -6,7 +6,7 @@ experiments_exec = 'LevmarExperiments.py'
 experiments_path = os.path.join(Paths.package_path, 'Scripts/MoreScripts/experiments/Levmar17/', experiments_exec)
 
 
-for i in range(970):
+for i in range(310):
     command_ = "python {0} {1} {2}".format(experiments_path, i, dry_run)
     #print "Calling:", command_
     os.system(command_)
